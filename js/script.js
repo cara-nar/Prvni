4c04ffb9b1425858da65c8612e5e43351bc4943e
cara-nar <68274798+cara-nar@users.noreply.github.com>
2020-07-16 14:03:51 +0200

    kk

--- js/script.js
+++ js/script.js
@@ -3,25 +3,24 @@ let i = 0;
 
 
 function uloz(coUlozit) {
-   let input = document.getElementById(coUlozit).value;
-   pole.push(input);
-   console.log(pole);
-    window.localStorage.setItem('ha',pole);
+    let input = document.getElementById(coUlozit).value;
+    pole.push(input);
+    console.log(pole);
+    window.localStorage.setItem('ha', pole);
 }
 
-function myF() 
-{
+function myF() {
     document.getElementById("show").classList.toggle("show");
 }
 
 function vypis(text) {
     let sablona = document.getElementById('name');
     window.localStorage.
-    salona.innerHTML= "<p>" + text + "</p>";
+    salona.innerHTML = "<p>" + text + "</p>";
 }
 
-function zobraz () {
-    
+function zobraz() {
+
     console.log(pole[i]);
     vypis(pole[i]);
     window.localStorage.getItem(pole[i]);
@@ -29,30 +28,9 @@ function zobraz () {
 
 let counter = 0;
 
-function like(){
+function like() {
     counter++;
     console.log(counter);
     let inputs = document.getElementsByClassName("pocet");
     console.log(inputs);
-}
-
-
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
